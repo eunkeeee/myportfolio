5a7a022122b76534d9b3e5421521d5586a63ce8b
eunkeeee <eunkeeeee@gmail.com>
2022-08-26 14:08:00 +0900

    toggle과 반응형 헤더 관련 해결!!!

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,37 +1,33 @@
 import styles from "./Header.module.css";
 import NavigationBar from "../components/NavigationBar";
-import { useState, useRef } from "react";
+import { useState } from "react";
+import useWindowDimensions from "../functions/useWindowDimensions";
 
-const Header = ({ onClick }) => {
-  const listElement = useRef(null);
-  const [isLargeEnough, setLargeEnough] = useState();
+const Header = () => {
+  const [menu, setMenu] = useState(true);
   const onResize = () => {
-    if (window.innerWidth < 1100) {
-      setLargeEnough(false);
+    const { innerWidth: width, innerHeight: height } = window;
+    if (width < 768) {
+      setMenu(false);
     } else {
-      setLargeEnough(true);
+      setMenu(true);
     }
   };
-
+  window.addEventListener("resize", onResize);
   return (
     <div>
-      <header className={styles.Header} onClick={onClick}>
+      <header className={styles.Header}>
         <div className={styles.content}>
           <a href="#top">
             <div className={styles.title}>CEK's Portfolio</div>
           </a>
-          <NavigationBar />
+          {menu ? <NavigationBar /> : null}
 
           <a
             href="#"
             className={styles.toggleBtn}
             onClick={() => {
-              const menus = document.querySelector(
-                `.${styles.navigation__menus}`
-              );
-              console.log(menus.classList[0]);
-              const realMenus = document.querySelector(menus.classList[0]);
-              realMenus.classList.add(".active");
+              setMenu((current) => !current);
             }}
           >
             <i class="fa-solid fa-bars"></i>
