b1862c6ad04cd9aec6c64b8a2560f3c066b83416
eunkeeee <eunkeeeee@gmail.com>
2022-09-12 17:18:59 +0900

    useMediaQuery 훅으로 반응형 props 넘겨주기

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ import Skills from "./screens/Skills";
 import Archiving from "./screens/Archiving";
 import Project from "./screens/Project";
 import Footer from "./screens/Footer";
+import { useMediaQuery } from "react-responsive";
 
 function App() {
+  const isPc = useMediaQuery({
+    query: "(min-width:1024px)",
+  });
+  const isTablet = useMediaQuery({
+    query: "(min-width:768px) and (max-width:1023px)",
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-width:767px)",
+  });
+
   const isWideEnough = () => {};
   const [appearBtn, setAppear] = useState(false);
   const scrollDown = () => {
@@ -24,25 +35,25 @@ function App() {
     <div className="app">
       <div id="top"></div>
       <div id="section-Header">
-        <Header />
+        <Header isPc={isPc} isTablet={isTablet} isMobile={isMobile} />
       </div>
       <div id="section-Mainhead">
-        <Mainhead />
+        <Mainhead isPc={isPc} isTablet={isTablet} isMobile={isMobile} />
       </div>
       <div id="section-AboutMe">
-        <AboutMe />
+        <AboutMe isPc={isPc} isTablet={isTablet} isMobile={isMobile} />
       </div>
       <div id="section-Skills">
-        <Skills />
+        <Skills isPc={isPc} isTablet={isTablet} isMobile={isMobile} />
       </div>
       <div id="section-Archiving">
-        <Archiving />
+        <Archiving isPc={isPc} isTablet={isTablet} isMobile={isMobile} />
       </div>
       <div id="section-Project">
-        <Project />
+        <Project isPc={isPc} isTablet={isTablet} isMobile={isMobile} />
       </div>
       <div id="section-Footer">
-        <Footer />
+        <Footer isPc={isPc} isTablet={isTablet} isMobile={isMobile} />
       </div>
 
       {appearBtn ? (
